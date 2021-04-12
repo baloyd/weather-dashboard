b23a28c651205628ab87d2ee4b6eb6d9c354de2e
baloyd <danielbaloy@gmail.com>
2021-04-11 20:39:45 -0400

    minor fixes;click event for search history debugged.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,18 +1,99 @@
 //global variables
-var API_KEY="af9615c5a33d02dcff997f71632442e3";
+var API_KEY = "af9615c5a33d02dcff997f71632442e3";
 var currentDate = moment().format("M/D/YYYY")
 
+   //fetches information from the weather api. If there is a response, then the data will be available in the console.
+   function getWeather(cityName) {
+       //request url to open weather api with the concatenated city inputted by the user.
+       var requestUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&units=imperial&appid=" + API_KEY;
+
+     fetch(requestUrl).then(function (response) {
+        if (!response.ok) {
+            alert("No information found for " + cityName);
+        }
+        return response.json();
+
+    }).then(function (data) {
+        console.log(data);
+   
+        //request url that uses the latitude and longitude to retrieve a 7-day forecast
+        var latitude = data[0].lat;
+        var longitude = data[0].lon;
+        var forecastRequestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=" + API_KEY
+
+        fetch(forecastRequestUrl).then(function (response) {
+            if (!response.ok) {
+                alert("No forecast found for " + cityName);
+            }
+            return response.json()
+        }).then(function (data) {
+            console.log(data);
+
+            //variables that use the info from the object to then place the information in the current weather column.
+            var dailyTemp = data.daily[0].temp.day
+            var dailyHumid = data.daily[0].humidity
+            var dailyWind = data.daily[0].wind_speed
+            var dailyIndex = data.daily[0].uvi
+            var weatherIcon = data.daily[0].weather[0].icon
+            var iconImage = $("<img>");
+            iconImage.attr("src", "http://openweathermap.org/img/w/" + weatherIcon + ".png")
+
+
+
+            //adding the values pulled from the object to populate the placeholders
+            $("#cityHolder").text(cityName + " (" + currentDate + ")")
+            iconImage.appendTo("#cityHolder")
+            $("#tempHolder").text("Temperature: " + dailyTemp + " °F")
+            $("#humHolder").text("Humidity: " + dailyHumid + "%")
+            $("#windHolder").text("Wind Speed: " + dailyWind + " MPH")
+            $("#uvHolder").html("UV Index: <span id='number'>" + dailyIndex + "</span>")
+
+            //applies the appropriate classes depending on what the uv index is for the given city.
+            if (dailyIndex <= 2) {
+                $("#number").removeClass()
+                $("#number").addClass("favorable")
+            } else if (dailyIndex >= 8) {
+                $("#number").addClass("severe")
+            } else
+                $("#number").addClass("moderate")
+
+           
+            //formula to loop through the array to grab the necessary information to create a 5-day forecast and then append it to the forecast column
+            for (var i = 1; i < 6; i++) {
+                var dailyCast = data.daily[i];
+
+                var castDiv = $("<div>");
+                castDiv.addClass("forecastCard");
+
+                listDay = $("<div>")
+                listDay.text(moment().add(i, "days").format("M/D/YYYY"))
+                listDay.appendTo(castDiv)
+
+                listIcon = $("<img>")
+                listIcon.attr("src", "http://openweathermap.org/img/w/" + dailyCast.weather[0].icon + ".png")
+                listIcon.appendTo(castDiv)
+
+                listTemp = $("<div>")
+                listTemp.text("Temp: " + dailyCast.temp.day + "°F")
+                listTemp.appendTo(castDiv)
+
+                listHumid = $("<div>")
+                listHumid.text("Humidity: " + dailyCast.humidity + "%")
+                listHumid.appendTo(castDiv)
+
+                castDiv.appendTo("#forecast")
+
+            }
+        })
+    })
+}
 //click event that grabs user input and creates a list of user search history.
-$("button").on("click",function(event) {
+$("button").on("click", function (event) {
     event.preventDefault();
-    var cityName=$("#cityNameInput").val();
-    if(!cityName) {
+    var cityName = $("#cityNameInput").val();
+    if (!cityName) {
         alert("Please enter the name of a city.");
     } else {
-
-        //request url to open weather api with the concatenated city inputted by the user.
-        var requestUrl= "https://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&units=imperial&appid="+API_KEY;
-       
         var cityList = $("<li>")
         cityList.addClass("list-group-item")
         cityList.text(cityName);
@@ -20,93 +101,11 @@ $("button").on("click",function(event) {
         localStorage.setItem("cityName", cityName)
         $("#forecast").empty()
         getWeather(cityName)
-       
-        //fetches information from the weather api. If there iis a response, then the data will be available in the console.
-        function getWeather(cityName){
-            fetch(requestUrl).then(function(response) {
-            if(!response.ok) {
-                alert("No information found for "+cityName);
-            } 
-            return response.json();
-            
-        }).then(function(data) {
-            console.log(data);
-                
-            //request url that uses the latitude and longitude to retrieve a 7-day forecast
-                var latitude=data[0].lat;
-                var longitude=data[0].lon;
-                var forecastRequestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&units=imperial&appid="+API_KEY
-
-                fetch(forecastRequestUrl).then(function(response){
-                    if(!response.ok) {
-                        alert("No forecast found for "+cityName);
-                    }
-                    return response.json()
-                }).then(function(data){
-                    console.log(data);
-                
-                    //variables that use the info from the object to then place the information in the current weather column.
-                    var dailyTemp = data.daily[0].temp.day
-                    var dailyHumid = data.daily[0].humidity
-                    var dailyWind = data.daily[0].wind_speed
-                    var dailyIndex = data.daily[0].uvi
-                    var weatherIcon = data.daily[0].weather[0].icon
-                    var iconImage = $("<img>");
-                    iconImage.attr("src","http://openweathermap.org/img/w/"+weatherIcon+".png")
-                    
-                   
-                
-                   //adding the values pulled from the object to populate the placeholders
-                    $("#cityHolder").text(cityName+" ("+currentDate+")")
-                    iconImage.appendTo("#cityHolder")
-                    $("#tempHolder").text("Temperature: "+dailyTemp+"°F")
-                    $("#humHolder").text("Humidity: "+dailyHumid+"%")
-                    $("#windHolder").text("Wind Speed: "+dailyWind+"MPH")
-                    $("#uvHolder").html("UV Index: <span id='number'>"+dailyIndex+"</span>")
-                    
-                    //applies the appropriate classes depending on what the uv index is for the given city.
-                    if(dailyIndex <= 2){
-                        $("#number").removeClass() 
-                        $("#number").addClass("favorable")
-                    }else if(dailyIndex >=8) {
-                        $("#number").addClass("severe")
-                    }else 
-                    $("#number").addClass("moderate")
-                    
-                  //when city in search history is clicked, the weather is displayed on the right.
-                   $("#ul").on("click",function(event) {
-                   var listVal = event.target.textContent
-                     console.log("clicked")
-                     $("#forecast").empty()
-                      getWeather(listVal)
-                        })
-                    //formula to loop through the array to grab the necessary information to create a 5-day forecast and then append it to the forecast column
-                    for(var i=1;i<6;i++) {
-                        var dailyCast=data.daily[i];
-                       
-                        var castDiv=$("<div>");
-                        castDiv.addClass("forecastCard");
-
-                        listDay=$("<div>")
-                        listDay.text(moment().add(i,"days").format("M/D/YYYY"))
-                        listDay.appendTo(castDiv)
-
-                         listIcon=$("<img>")
-                         listIcon.attr("src","http://openweathermap.org/img/w/"+dailyCast.weather[0].icon+".png")
-                         listIcon.appendTo(castDiv)
-
-                        listTemp=$("<div>")
-                        listTemp.text("Temp: "+dailyCast.temp.day+"°F")
-                        listTemp.appendTo(castDiv)
-
-                        listHumid=$("<div>")
-                        listHumid.text("Humidity: "+dailyCast.humidity+"%")
-                        listHumid.appendTo(castDiv)
-                         
-                        castDiv.appendTo("#forecast")
-
-                }})})}}})
-
-           
- 
- 
+    }  
+})
+$("ul").on("click", function (event) {
+    var listVal = event.target.textContent
+    console.log("clicked")
+    $("#forecast").empty()
+    getWeather(listVal)
+})
